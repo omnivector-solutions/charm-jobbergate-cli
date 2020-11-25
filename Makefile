d1abaf04c2bcbb414735558ab1067b4beeea31c6
Cory Dodt <121705+corydodt@users.noreply.github.com>
2020-11-25 09:19:58 -0800

    175858656 minor repo cleanup, formatting, ignoring

--- Makefile
+++ Makefile
@@ -1,23 +1,33 @@
+.ONESHELL:
+.DEFAULT_GOAL := help
+SHELL := /bin/bash
+
 export PATH := /snap/bin:$(PATH)
-  
+
+
+jobbergate.charm: src/charm.py metadata.yaml .jujuignore
+	$(MAKE) clean
+	charmcraft build
+
+
 # TARGETS
 lint: ## Run linter
 	tox -e lint
 
+
 clean: ## Remove .tox and build dirs
-	rm -rf .tox/
-	rm -rf venv/
+	rm -rf .tox/ venv/ build/
+	rm -f jobbergate.charm
+
 
 push-charm-to-edge: ## Remove .tox and build dirs
-	aws s3 cp jobbergate.charm s3://omnivector-public-assets/charms/charm-jobbergate/edge/ 
+	aws s3 cp jobbergate.charm s3://omnivector-public-assets/charms/charm-jobbergate/edge/
+
 
 pull-charm-from-edge: ## Remove .tox and build dirs
 	wget https://omnivector-public-assets.s3-us-west-2.amazonaws.com/charms/charm-jobbergate/edge/jobbergate.charm
 
-# SETTINGS
-# Use one shell for all commands in a target recipe
-.ONESHELL:
-# Set default goal
-.DEFAULT_GOAL := help
-# Use bash shell in Make instead of sh 
-SHELL := /bin/bash
+
+format:
+	isort src
+	black src
--- src/charm.py
+++ src/charm.py
@@ -4,9 +4,7 @@ import subprocess
 
 from ops.charm import CharmBase
 from ops.main import main
-from ops.model import (
-    ActiveStatus
-)
+from ops.model import ActiveStatus
 
 
 class CharmJobbergate(CharmBase):
@@ -17,36 +15,37 @@ class CharmJobbergate(CharmBase):
         super().__init__(*args)
 
         event_handler_bindings = {
-            self.on.install:
-            self._on_install,
-
-            self.on.upgrade_charm:
-            self._on_upgrade_charm,
+            self.on.install: self._on_install,
+            self.on.upgrade_charm: self._on_upgrade_charm,
         }
         for event, handler in event_handler_bindings.items():
             self.framework.observe(event, handler)
 
     def _on_install(self, event):
         """Install the jobbergate-cli snap."""
-        subprocess.run([
-            "snap",
-            "install",
-            self.model.resources.fetch('jobbergate-snap'),
-            "--dangerous",
-            "--classic"
-        ])
+        subprocess.run(
+            [
+                "snap",
+                "install",
+                self.model.resources.fetch("jobbergate-snap"),
+                "--dangerous",
+                "--classic",
+            ]
+        )
         self.unit.status = ActiveStatus("Jobbergate Installed")
 
     def _on_upgrade_charm(self, event):
         """Upgrade the charm."""
         # place holder until snap available
-        subprocess.run([
-            "snap",
-            "refresh",
-            self.model.resources.fetch('jobbergate-snap'),
-            "--dangerous",
-            "--classic"
-        ])
+        subprocess.run(
+            [
+                "snap",
+                "refresh",
+                self.model.resources.fetch("jobbergate-snap"),
+                "--dangerous",
+                "--classic",
+            ]
+        )
         self.unit.status = ActiveStatus("Jobbergate upgraded")
 
 
